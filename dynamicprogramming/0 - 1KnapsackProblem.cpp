0358281fd9150efadd567019ad1462480c6a3069
Abhishek Tyagi <abhishektyagi2912@gmail.com>
2022-10-07 16:28:15 +0530

    Create 0 - 1KnapsackProblem.cpp

---
+++ dynamicprogramming/0 - 1KnapsackProblem.cpp
@@ -0,0 +1,41 @@
+/* A Naive recursive implementation of
+0-1 Knapsack problem */
+#include <bits/stdc++.h>
+using namespace std;
+
+// A utility function that returns
+// maximum of two integers
+int max(int a, int b) { return (a > b) ? a : b; }
+
+// Returns the maximum value that
+// can be put in a knapsack of capacity W
+int knapSack(int W, int wt[], int val[], int n)
+{
+
+	if (n == 0 || W == 0)
+		return 0;
+
+
+	if (wt[n - 1] > W)
+		return knapSack(W, wt, val, n - 1);
+
+	
+	else
+		return max(
+			val[n - 1]
+				+ knapSack(W - wt[n - 1],
+						wt, val, n - 1),
+			knapSack(W, wt, val, n - 1));
+}
+
+// Driver code
+int main()
+{
+	int val[] = { 60, 100, 120 };
+	int wt[] = { 10, 20, 30 };
+	int W = 50;
+	int n = sizeof(val) / sizeof(val[0]);
+	cout << knapSack(W, wt, val, n);
+	return 0;
+}
+
