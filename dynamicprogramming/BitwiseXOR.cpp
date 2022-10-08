6754e7c70736ab0012cc0f1dcf473f17522aaad1
Mridul Sharma <91523026+Mridul07Sharma@users.noreply.github.com>
2022-10-08 20:32:57 +0530

    Create BitwiseXOR.cpp

---
+++ dynamicprogramming/BitwiseXOR.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+ 
+
+int main() {
+ int a = 5, b = 9;
+ cout<<"a = " << a <<","<< " b = " << b <<endl;
+ cout << "a & b = " << (a & b) << endl;
+ cout << "a | b = " << (a | b) << endl;
+ cout << "a ^ b = " << (a ^ b) << endl;
+ cout << "~a = " << (~a) << end;
+ cout<<"b << 1" <<" = "<< (b << 1) <<endl;
+ cout<<"b >> 1 "<<"= " << (b >> 1 )<<endl;
+ return 0;
+}
