9e2c133e37dfac103fa9935e577490556cfa5624
parinayrana <115211979+parinayrana@users.noreply.github.com>
2022-10-09 22:36:00 +0530

    Create destructors.py

---
+++ PYTHON/destructors.py
@@ -0,0 +1,13 @@
+# Python program to illustrate destructor
+class Employee:
+
+	# Initializing
+	def __init__(self):
+		print('Employee created.')
+
+	# Deleting (Calling destructor)
+	def __del__(self):
+		print('Destructor called, Employee deleted.')
+
+obj = Employee()
+del obj
